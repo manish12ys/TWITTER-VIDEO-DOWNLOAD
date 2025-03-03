8c8d653a9bb0c62c3071bc2b61eecc8caae61b91
manish12ys <manishchowdary2006@gmail.com>
2025-03-03 10:49:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,4 +43,5 @@ def serve_download(filename):
     return send_file(os.path.join(DOWNLOAD_FOLDER, filename), as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
+
